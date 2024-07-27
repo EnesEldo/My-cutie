252d52c9d9cf99ddac213818dd5789cf80a1f80a
EnesEldo <eneseldo@gmail.com>
2024-07-28 00:36:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const messages = [
     "You might be asking why did i put alot of effort into this...",
     "It's because I Love You and I wanna cheer you up by making this website which looks simple but meaningful",
     "With this i wanted to remind you, I will and try to be the best for you and you will never be scared with me as long we try alright? "
+    "I want you to know that you are incomparable and unique, and I love you just as you are, you bring so much joy into my life, and I am so grateful for you, no one should ever compare you because you are one of a kind.""
 ];
 
 const additionalMessages = [
-    "You are unique and special to me,!",
-    "Never forget that i'm always here for you!",
+    "You deserve all the effort from me and i will not stop until the end of our life!",
+    "Never forget that i'm always here for you, Don't be scared with me ya?",
     "And promise me... be alive for me would ya?",
-    "Promise me to love and remember me",
+    "Promise me to love and remember me...",
     "Deep down I truly Love You no matter what from my bottom of my heart to the top!!",
     "May God bless our relationship, our journey together, you and i will have a great blessed path" 
+    "I Love You in Every Universe"
 ];
 
 let messageIndex = 0;
